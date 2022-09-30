fa2fb09eb0b587e9a0073c331a1a089303ca3e70
MarcosSilva13 <marcosslp45@gmail.com>
2022-09-30 18:18:04 -0300

    Ajuste e criação do método inserirAresta

--- src/Trabalho/Grafo.java
+++ src/Trabalho/Grafo.java
@@ -31,28 +31,46 @@ public class Grafo {
                 matCoor[i][0] = linha;
                 matCoor[i][1] = coluna;
             }
-
-            /*for (int i = 0; i < this.numVertices; i++) {
-                System.out.println(matCoor[i][0] + " " + matCoor[i][1]);
-            }*/
             numArestas = Integer.parseInt(br.readLine());
-            //System.out.println(this.numArestas);
 
             if (verificaDirecionado()) {
                 criaDirecionado();
             } else {
                 criaNaoDirecionado();
             }
-
         } catch (IOException ex) {
             ex.printStackTrace();
+        }
+    }
+
+    public void inserirAresta(int origem, int destino, int p) {
+        if (p >= 0) { //mudar para > 0 caso não possa add peso zero
+            if (origem < 0 || origem >= numVertices || destino < 0 || destino >= numVertices) {
+                System.out.println("Posição inválida!\n");
+            } else if (matGrafo[origem][destino] != -1) {
+                System.out.println("Posição já ocupada!\n");
+            } else if (verificaDirecionado()) {
+                matGrafo[origem][destino] = p;
+                numArestas++;
+                System.out.println("Aresta inserida com sucesso!\n");
+                testeImprimir(); //remover depois
+            } else {
+                matGrafo[origem][destino] = p;
+                matGrafo[destino][origem] = p;
+                //numArestas+= 2; usar só se for salvar a matriz espelhada
+                numArestas++;
+                System.out.println("Aresta inserida com sucesso!\n");
+                testeImprimir(); //remover depois
+            }
+        } else {
+            System.out.println("Peso não pode ser negativo!\n");
         }
     }
 
     private void criaNaoDirecionado() {
         inicializaMatriz();
         try {
-            for (int i = 0; i < numArestas; i++) {
+            for (int i = 0; i < numVertices; i++) {
                 st = new StringTokenizer(br.readLine());
                 linha = Integer.parseInt(st.nextToken());
                 coluna = Integer.parseInt(st.nextToken());
@@ -65,9 +83,9 @@ public class Grafo {
         }
 
         //testes
-        for (int i = 0; i < numArestas; i++) {
-            for (int j = 0; j < numArestas; j++) {
-                if (matGrafo[i][j] != -1){
+        for (int i = 0; i < numVertices; i++) {
+            for (int j = 0; j < numVertices; j++) {
+                if (matGrafo[i][j] != -1) {
                     System.out.println(i + " " + j + " " + matGrafo[i][j]);
                 }
             }
@@ -79,7 +97,7 @@ public class Grafo {
     private void criaDirecionado() {
         inicializaMatriz();
         try {
-            for (int i = 0; i < numArestas; i++) {
+            for (int i = 0; i < numVertices; i++) {
                 st = new StringTokenizer(br.readLine());
                 linha = Integer.parseInt(st.nextToken());
                 coluna = Integer.parseInt(st.nextToken());
@@ -91,9 +109,9 @@ public class Grafo {
         }
 
         //testes
-        for (int i = 0; i < numArestas; i++) {
-            for (int j = 0; j < numArestas; j++) {
-                if (matGrafo[i][j] != -1){
+        for (int i = 0; i < numVertices; i++) {
+            for (int j = 0; j < numVertices; j++) {
+                if (matGrafo[i][j] != -1) {
                     System.out.println(i + " " + j + " " + matGrafo[i][j]);
                 }
             }
@@ -107,14 +125,16 @@ public class Grafo {
             BufferedWriter bw = new BufferedWriter(new FileWriter("novoGrafo.txt"));
             bw.write(direcionado + "\n");
             bw.write(numVertices + "\n");
+
             for (int i = 0; i < numVertices; i++) {
                 bw.write(i + " " + matCoor[i][0] + " " + matCoor[i][1] + "\n");
             }
-            bw.write(numVertices + "\n");
+
+            bw.write(numArestas + "\n");
 
             if (verificaDirecionado()) {
-                for (int i = 0; i < numArestas; i++) {
-                    for (int j = 0; j < numArestas; j++) {
+                for (int i = 0; i < numVertices; i++) {
+                    for (int j = 0; j < numVertices; j++) {
                         if (matGrafo[i][j] != -1) {
                             bw.write(i + " " + j + " " + matGrafo[i][j] + "\n");
                         }
@@ -129,33 +149,34 @@ public class Grafo {
                     }
                 }
             }
-
             bw.close();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
-    private void inicializaMatriz(){ //usar só se o peso puder ser zero
-        matGrafo = new int[numArestas][numArestas];
 
-        for (int i = 0; i < numArestas; i++) {
-            for (int j = 0; j < numArestas; j++) {
+    private void inicializaMatriz() { //usar só se o peso puder ser zero
+        matGrafo = new int[numVertices][numVertices];
+
+        for (int i = 0; i < numVertices; i++) {
+            for (int j = 0; j < numVertices; j++) {
                 matGrafo[i][j] = -1;
             }
         }
     }
+
     private boolean verificaDirecionado() {
         return (direcionado.equals("direcionado=sim"));
     }
 
     private void testeImprimir() { // apagar depois pois é so para testar a matriz
-        for (int i = 0; i < numArestas; i++) {
+        for (int i = 0; i < numVertices; i++) {
             System.out.print("   " + i + " ");
         }
         System.out.println();
-        for (int i = 0; i < numArestas; i++) {
+        for (int i = 0; i < numVertices; i++) {
             System.out.print(i + "| ");
-            for (int j = 0; j < numArestas; j++) {
+            for (int j = 0; j < numVertices; j++) {
 
                 System.out.print(matGrafo[i][j] + "   ");
             }
